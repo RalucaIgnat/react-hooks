bb2e6986da8aa0107c1ae988a07c61f561cfcf3b
Raluca <ignatraluca93@gmail.com>
2023-03-01 11:24:13 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { FaStar } from "react-icons/fa";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const createArray = (length) => [...Array(length)];
+
+function Star() {
+  return <FaStar />;
+}
+
+function StarRating(totalStars) {
+  return createArray(totalStars).map((n, i) => <Star key={i} />);
+}
+
+function App() {
+  return <StarRating totalStars={10} />;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
