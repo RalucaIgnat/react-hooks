f72b1d609535d937ca3eecabf35d5a8603679a64
Raluca <ignatraluca93@gmail.com>
2023-03-01 16:52:47 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import { useTrees } from "./index.js";
 
-function App({name}) {
+function App() {
+  const { trees } = useTrees();
   return (
-    <div className="App">
-       <h1>Hello {name} </h1>
+    <div>
+      <h1>Trees I ve heard of </h1>
+      <ul>
+        {trees.map((tree) => (
+          <li key={tree.id}>{tree.type}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,20 @@
-import React, { useState } from "react";
+import React, { createContext, useContext } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { useInput } from "./useinput";
+import App from "./App.js";
 
-function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useInput("#000000");
-
-  const submit = (e) => {
-    e.preventDefault();
-
-    alert(`${titleProps.value} sounds like ${colorProps.value}`);
-    resetTitle();
-    resetColor();
-  };
-  return (
-    <form onSubmit={submit}>
-      <input {...titleProps} type="text" placeholder="Sound..." />
-      <input {...colorProps} type="color" />
-      <button>ADD</button>
-    </form>
-  );
-}
+const TreesContext = createContext();
+export const useTrees = () => useContext(TreesContext);
+const trees = [
+  { id: "1", type: "Maple" },
+  { id: "2", type: "Oak" },
+  { id: "3", type: "Family" },
+  { id: "4", type: "Component" },
+];
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <TreesContext.Provider value={trees}>
     <App />
-  </React.StrictMode>
+  </TreesContext.Provider>
 );
