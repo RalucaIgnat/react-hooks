ca6201ab6420c4a3d2931aed272d6c6226b898ac
Raluca <ignatraluca93@gmail.com>
2023-03-01 15:58:35 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { FaStar } from "react-icons/fa";
-
-const createArray = (length) => [...Array(length)];
-
-function Star() {
-  return <FaStar />;
-}
-
-function StarRating(totalStars) {
-  return createArray(totalStars).map((n, i) => <Star key={i} />);
-}
+import { useInput } from "./useinput";
 
 function App() {
-  return <StarRating totalStars={10} />;
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    alert(`${titleProps.value} sounds like ${colorProps.value}`);
+    resetTitle();
+    resetColor();
+  };
+  return (
+    <form onSubmit={submit}>
+      <input {...titleProps} type="text" placeholder="Sound..." />
+      <input {...colorProps} type="color" />
+      <button>ADD</button>
+    </form>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
